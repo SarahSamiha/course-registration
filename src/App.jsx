f06efdb0ed52f490b5fae62dc7bfc5b7c8813a51
Sarah Samiha Zaman <sarahfaizakhan@gmail.com>
2023-09-26 15:45:28 +0600

    made responsive for medium screen

--- src/App.jsx
+++ src/App.jsx
@@ -28,17 +28,17 @@ function App() {
 
   return (
     <div className='bg-slate-200'>
-      <div className='max-w-[360px] md:max-w-[1180px] mx-auto'>
+      <div className='max-w-[360px] md:max-w-[720px] lg:max-w-[1180px] mx-auto'>
         <header className='text-center pt-10 pb-10'>
           <Header></Header>
         </header>
-        <main className='flex flex-col-reverse md:flex-row gap-6 '>
-          <div className='md:w-2/3 lg:w-3/4'>
+        <main className='flex flex-col-reverse md:flex-row gap-6 md:gap-5 lg:gap-6 '>
+          <div className='md:w-4/5 lg:w-3/4'>
             <Courses
               handleSelectCourse={handleSelectCourse}
             ></Courses>
           </div>
-          <div className='md:1/3 lg:w-1/4 bg-white rounded-xl'>
+          <div className='md:1/5 lg:w-1/4 bg-white rounded-xl'>
             <MyCourses
               myCourses={myCourses}
               totalCreditHour={totalCreditHour}
